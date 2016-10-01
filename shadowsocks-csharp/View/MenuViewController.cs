b9781593d349c89a3e9a077344ebba340cf0c436
Xiaoru Li <ujewp@student.kit.edu>
2016-10-01 20:52:34 +0000

    Updated MenuViewController.cs

--- shadowsocks-csharp/View/MenuViewController.cs
+++ shadowsocks-csharp/View/MenuViewController.cs
@@ -134,8 +134,7 @@ namespace Shadowsocks.View
             {
                 serverInfo = config.GetCurrentServer().FriendlyName();
             }
-             we want to show more details but notify icon title is limited to 63 characters
-            //string text = I18N.GetString("Shadowsocks") + " " + UpdateChecker.Version + "\n" +
+            // we want to show more details but notify icon title is limited to 63 characters
             string text = I18N.GetString("Shadowsocks") + " 2.5.8 " + "\n" +
                 (enabled ?
                     I18N.GetString("System Proxy On: ") + (global ? I18N.GetString("Global") : I18N.GetString("PAC")) :
